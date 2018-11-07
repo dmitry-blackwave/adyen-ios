e9af569e501b5d67b90960b4e0df71448486c120
Dmitry Belov <dmitriy.belov@sportpursuit.co.uk>
2018-11-07 12:14:32 +0700

    one payment without select payment screen.

---
+++ Adyen/UI/Checkout/PreferredPaymentMethodDelegate.swift
@@ -0,0 +1,10 @@
+/// The `PreferredPaymentMethodDelegate` protocol defines the methods that a paymentMethodDelegate of `CheckoutViewController` should implement to provide preffered payment method.
+public protocol PreferredPaymentMethodDelegate: class {
+    /// Invoked when the payment methods are available and client can select the preffered one to continue with.
+    ///
+    /// - Parameters:
+    ///   - controller: The checkout view controller that finished the payment flow.
+    ///   - available: Available payment methods.
+    /// - Returns: Preferred payment method or nil then selection will be presented.
+    func preferredMethod(_ controller: CheckoutViewController, available availableMethods: [PaymentMethod]) -> PaymentMethod?
+}
